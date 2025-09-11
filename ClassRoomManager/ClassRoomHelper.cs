f0ddb414d67b4da800a67568b7df04331ad3eea9
jakeyjake07 <jaakob@live.se>
2025-09-11 17:47:49 +0200

    updated

--- ClassRoomManager/ClassRoomHelper.cs
+++ ClassRoomManager/ClassRoomHelper.cs
@@ -24,6 +24,8 @@ namespace ClassRoomManager
 
             students.Add(studentToAdd);
             System.Console.WriteLine($"Student {name} wad added to the list.");
+            System.Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
 
         }
 
@@ -69,14 +71,14 @@ namespace ClassRoomManager
             for (int i = 0; i < toolList.Length; i++)
             {
                 Tool t = toolList[i];
-                System.Console.WriteLine($"{i + 1}. {t.Name} {t.Difficulty} - {t.Description}");
+                System.Console.WriteLine($"{i + 1}. {t.Name} ({t.Difficulty}) - {t.Description}");
             }
 
             Console.Write("Choose (1-5): ");
             int choice = int.Parse(Console.ReadLine());
 
             Tool chosenTool = toolList[choice - 1];
-            int level = 5;
+            int level = chosenTool.Difficulty;
             int presentCount = 0;
 
             foreach (Student student in students)
@@ -93,6 +95,8 @@ namespace ClassRoomManager
             if (presentCount > 0)
             {
                 System.Console.WriteLine($"Lecture is finished: {chosenTool.Name}, {presentCount} taught. All records have been updated.");
+                System.Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
             }
             else
             {
@@ -124,6 +128,52 @@ namespace ClassRoomManager
 
 
         }
+
+
+        //Case 5
+
+        public static void ShowIndividual(List<Student> students)
+        {
+            if (students.Count == 0)
+            {
+                System.Console.WriteLine("No students in the list.");
+                return;
+            }
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                System.Console.WriteLine($"{i + 1}. {students[i].Name}");
+            }
+
+            System.Console.WriteLine($"Choose student: 1 - {students.Count} ");
+            int choice = int.Parse(Console.ReadLine());
+
+            Student student = students[choice - 1];
+            System.Console.WriteLine("--- Student Info ---");
+            System.Console.WriteLine($"Name: {student.Name}");
+            System.Console.WriteLine($"Level: {student.ProgramingLevel}/100");
+            System.Console.WriteLine($"Tools: ");
+
+            if (student.Tools.Count == 0)
+            {
+                System.Console.WriteLine("No tools learned.");
+            }
+
+            else
+            {
+                foreach (Tool tool in student.Tools)
+                {
+                    System.Console.WriteLine($"- {tool.Name} (Difficulty: {tool.Difficulty}) - {tool.Description}");
+                }
+            }
+
+            System.Console.WriteLine("----------------");
+            System.Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+        }
+
+
 
 
         //Case 6
