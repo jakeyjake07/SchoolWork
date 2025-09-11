43ca40f50357074bfa33c2cfcb6a44dfe6fb0f9b
jakeyjake07 <jaakob@live.se>
2025-09-11 17:50:38 +0200

    updated

--- Classes/obj/Debug/net8.0/Classes.AssemblyInfo.cs
+++ Classes/obj/Debug/net8.0/Classes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2dc2e3ae7acb36f98bda948ef79b47fde3f5ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2f3c43e2151362cc1f14ef0d5e5573db05268b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
